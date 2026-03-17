9e249cf08beef6040c8301e5984a2d4efe43f832
maria eduarda santos da silva <santos.silva.maria3108@escola.pr.gov.br>
2026-03-17 11:45:00 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+ <?php
+// //variável de controle
+// $i = 0;
+// //condição
+// while($i <= 100){
+//     echo "$i \n";
+//     //incremento
+//   $i = $i + 1;
+  
+// }
+// $usuario = readline ("Digite seu usuário: ");
+// $senha = readline ("Digite sua senha: ");
+// while($usuario != "admin" || $senha != "1234"){
+// echo "usuario  ou senha incorreto ";
+// $usuario = readline ("Digite seu usuário: ");
+// $senha = readline ("Digite sua senha: ");
+// }
+// echo "bem-vindo $usuario";
+
+// echo "calculadora de tabuada\n";
+// $num = readline("Digite um número: ");
+// $i = 0;
+// while($i <= 10){
+//   echo $num . "*" . $i . " = " .  $num * $i . "\n";
+//   $i++;
+//   }
+
+// echo "fatorial";
+// $numero = readline("Digite um número: ");
+
+// $contador = 1;
+// $fatorial = 1;
+
+// if ($numero < 0) {
+//     echo "Não existe fatorial de número negativo.";
+// } else {
+
+//     while ($contador <= $numero) {
+//         $fatorial = $fatorial * $contador;
+//         $contador++;
+//     }
+
+//     echo "O fatorial de $numero é: $fatorial";
+// }
+//   ?>
