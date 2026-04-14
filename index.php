4af65f7d91a46dcff6003a62f72b2659a3d952cf
maria eduarda santos da silva <santos.silva.maria3108@escola.pr.gov.br>
2026-04-14 11:01:42 +0000

    .

--- index.php
+++ index.php
@@ -1,45 +1,84 @@
- <?php
-// //variável de controle
-// $i = 0;
-// //condição
-// while($i <= 100){
-//     echo "$i \n";
-//     //incremento
-//   $i = $i + 1;
-  
-// }
-// $usuario = readline ("Digite seu usuário: ");
-// $senha = readline ("Digite sua senha: ");
-// while($usuario != "admin" || $senha != "1234"){
-// echo "usuario  ou senha incorreto ";
-// $usuario = readline ("Digite seu usuário: ");
-// $senha = readline ("Digite sua senha: ");
-// }
-// echo "bem-vindo $usuario";
 
-// echo "calculadora de tabuada\n";
-// $num = readline("Digite um número: ");
-// $i = 0;
-// while($i <= 10){
-//   echo $num . "*" . $i . " = " .  $num * $i . "\n";
-//   $i++;
-//   }
+<?php
 
-// echo "fatorial";
-// $numero = readline("Digite um número: ");
+// EXERCÍCIO 01
+//$i = 1;
+//while($i <= 500){
+//echo "$i\n";
+//$i++;
+//}
+//echo "\n";
 
-// $contador = 1;
-// $fatorial = 1;
+// EXERCÍCIO 02
+//$i = 20;
+//while($i >= 1){
+//echo "$i\n";
+//$i--;
+//}
+//echo "\n";
 
-// if ($numero < 0) {
-//     echo "Não existe fatorial de número negativo.";
-// } else {
+// EXERCÍCIO 03
+//$num = readline("Digite um número: ");
+//$i = 1;
+//while($i <= 10){
+//echo "$num x $i = " . ($num * $i) . "\n";
+//$i++;
+//}
+//echo "\n";
 
-//     while ($contador <= $numero) {
-//         $fatorial = $fatorial * $contador;
-//         $contador++;
-//     }
+// EXERCÍCIO 04
+//$i = 1;
+//$soma = 0;
+//while($i <= 100){
+//$soma += $i;
+//$i++;
+//}
+//echo "Soma: $soma\n";
+//echo "\n";
 
-//     echo "O fatorial de $numero é: $fatorial";
-// }
-//   ?>
+// EXERCÍCIO 05
+//$numero = readline("Digite um número: ");
+//$fatorial = 1;
+
+//for($i = $numero; $i > 1; $i--){
+//$fatorial *= $i;
+//}
+//echo "$numero! = $fatorial\n";
+//echo "\n";
+
+// EXERCÍCIO 06
+//$op = 0;
+//while($op != 4){
+//echo "1-Home\n2-Cadastro\n3-Login\n4-Sair\n";
+//$op = readline("Escolha: ");
+//}
+//echo "Saiu do sistema\n";
+//echo "\n";
+
+// EXERCÍCIO 07
+//$soma = 0;
+//$num = 1;
+//while($num != 0){
+//$num = readline("Digite um número (0 para sair): ");
+//if($num != 0){
+//$soma += $num;
+//}
+//}
+//echo "Soma total: $soma\n";
+//echo "\n";
+
+// EXERCÍCIO 08
+//$segredo = 50;
+//$palpite = 0;
+//while($palpite != $segredo){
+//$palpite = readline("Tente adivinhar: ");
+//if($palpite > $segredo){
+//echo "Muito alto\n";
+//}elseif($palpite < $segredo){
+//echo "Muito baixo\n";
+//}else{
+//echo "Acertou!\n";
+//}
+//}
+
+?>
